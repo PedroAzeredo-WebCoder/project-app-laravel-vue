09a0a4e7e31521da12dd96c36e716999fa468b93
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-14 10:03:58 -0300

    feat: created migration of product branch tables, with their respective rollbacks and foreign keys

---
+++ database/migrations/2025_05_09_193915_ajuste_produtos_filiais.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filiais', function (Blueprint $table) {
+            $table->id();
+            $table->string('filial', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('produto_filiais', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('filial_id')->constrained('filiais');
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->decimal('preco', 8, 2);
+            $table->integer('quantidade');
+            $table->timestamps();
+
+            $table->foreignId('filial_id')->constrained('filiais');
+            $table->foreignId('produto_id')->constrained('produtos');
+        });
+
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropColumn('preco', 'quantidade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->decimal('preco', 8, 2);
+            $table->integer('quantidade');
+        });
+
+        Schema::dropIfExists('produto_filiais');
+        Schema::dropIfExists('filiais');
+    }
+};
